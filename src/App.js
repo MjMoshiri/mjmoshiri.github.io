03b4cba0f7dbdcef537837bcfc2c32163dbcbae8
MJ <thisismjmoshiri@gmail.com>
2023-10-26 00:19:19 +0300

    add Landing section content

--- src/App.js
+++ src/App.js
@@ -8,21 +8,20 @@ import Education from './components/Education';
 import Projects from './components/Projects';
 import Courses from './components/Courses';
 import Contact from './components/Contact';
+
 function App() {
-  return (
-    <div>
-      <Navbar />
-      <Landing />
-      <Container>
-        <AboutMe />
-        <Works/>
-        <Projects/>
-        <Education/>
-        <Courses/>
-      </Container>
-      <Contact/>
-    </div>
-  );
+    return (<div>
+        <Navbar/>
+        <Landing/>
+        <Container>
+            <AboutMe/>
+            <Works/>
+            <Projects/>
+            <Education/>
+            <Courses/>
+        </Container>
+        <Contact/>
+    </div>);
 }
 
 export default App;
--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
-
+import './index.css';
 function Landing() {
     return (
-        <div>
-            <h2>Landing</h2>
-            <p>
-               Placeholder text.
-            </p>
+        <div className="landing-container">
+            <img className="landing-image" src="/me.jpg" alt="MJ"/>
+            <div className="landing-text-container">
+                <h1>Hello, I'm <span>MJ</span></h1>
+                <p>
+                    An all-around Software Engineer with a passion for <span className="animated-text">problem solving</span> and learning new technologies.
+                </p>
+            </div>
         </div>
     );
 }
+
 
 export default Landing;
