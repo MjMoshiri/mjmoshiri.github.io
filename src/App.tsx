1059d03dec7c3f35d2552b9eeb4d368e81480c0b
MJ Moshiri <mohammadjavad.moshiri@tuni.fi>
2023-11-20 17:23:53 +0200

    switch BrowserRouter with HashRouter for static development

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useMemo, useState} from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {HashRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
 import TrieData from "./data/trie-data";
 import {IDataItem, Category} from "./types";
